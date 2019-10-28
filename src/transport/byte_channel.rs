38ba610715a166d6325c944d7d8830079facd839
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2019-10-28 21:36:00 +0800

    Factor out channel sub modules

---
+++ src/transport/byte_channel.rs
@@ -0,0 +1,68 @@
+use std::{
+    convert::TryInto,
+    sync::mpsc::{Receiver, Sender},
+};
+
+use crate::{message::Message, transport::Transport};
+
+pub struct ByteChannel {
+    self_sender: Sender<Vec<u8>>,
+    senders: Vec<Sender<Vec<u8>>>,
+    receiver: Receiver<Vec<u8>>,
+}
+
+impl ByteChannel {
+    pub fn new(num_processes: usize) -> Vec<Box<dyn Transport>> {
+        let mut senders = vec![];
+        let mut receivers = vec![];
+        for _ in 0..num_processes {
+            let (sender, receiver) = std::sync::mpsc::channel();
+            senders.push(sender);
+            receivers.push(receiver);
+        }
+        let mut boxes = vec![];
+        for receiver in receivers
+            .into_iter()
+            .enumerate()
+            .map(|(i, receiver)| ByteChannel {
+                self_sender: senders[i].clone(),
+                senders: senders.clone(),
+                receiver,
+            })
+        {
+            let b: Box<dyn Transport> = Box::new(receiver);
+            boxes.push(b);
+        }
+        boxes
+    }
+}
+
+impl Transport for ByteChannel {
+    fn num_senders(&self) -> usize {
+        self.senders.len()
+    }
+
+    fn send(&self, message: Message) {
+        for sender in &self.senders {
+            let _ = sender
+                .send(message.clone().into())
+                .map_err(|e| eprintln!("Failed to send {:?}", e.0));
+        }
+    }
+
+    fn send_to_self(&self, message: Message) {
+        let _ = self
+            .self_sender
+            .send(message.clone().into())
+            .map_err(|e| eprintln!("Failed to send to self {:?}", e.0));
+    }
+
+    fn receive(&self) -> Message {
+        self.receiver
+            .recv()
+            .expect("recv")
+            .try_into()
+            .map_err(|e| eprintln!("Failed to parse: {}", e))
+            .expect("parse")
+    }
+}
